d3e4f609587c9cfc881787543295bb6bf72502c1
Asiphe <amhambi95@gmail.com>
2024-05-07 09:40:41 +0200

    added media icons

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './Search';
+import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
 
 
 // Body component
@@ -10,8 +11,22 @@ const Body = () => {
       <h1>One Million Success Stories <br /><br /> Start Yours Today With Us</h1>
 
       <Search />
+
+
+      <div className="media-icons">
+        <div className='facebook'>
+          <a href="https://www.facebook.com/profile.php?id=100092504071844"><FaFacebook style={{ color: '#3b5998', fontSize: '40px' }}/></a>
+        </div>
+        <div className='github'>
+          <a href="https://github.com/AsipheMhambi?tab=repositories"><FaGithub style={{ color: '#24292e', fontSize: '40px' }} /></a>
+        </div>
+        <div className='linkedin'>
+          <a href='www.linkedin.com/in/asiphe-mhambi-81504525a'><FaLinkedin  style={{color: '#0e76a8', fontSize: '40px'}}/></a>
+        </div>
+      </div>
       
     </div>
+    
   );
 };
 
